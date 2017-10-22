cb345aa10d00e9100e4895a3d4626a5dcedd800f
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2017-10-21 20:38:26 -0700

    Move some files.
    Reviewed by @tolmasky.

---
+++ isomorphic-compile/internal/project.js
@@ -0,0 +1,96 @@
+
+const { dirname, extname, join } = require("path");
+
+const { readdirSync, copyFileSync } = require("fs");
+const { execSync } = require("child_process");
+const mkdirp = path => execSync(`mkdir -p ${JSON.stringify(path)}`) && path;
+
+const { internalModuleStat } = process.binding("fs");
+const tstat = path => ["file", "directory"][internalModuleStat(path)];
+
+const micromatch = require("micromatch");
+const transform = require("./transform");
+
+
+module.exports = function project({ root, destination, cache, exclude, assets })
+{
+    mkdirp(dirname(destination));
+    mkdirp(cache);
+
+    return  <item   path = { root }
+                    cache = { cache }
+                    exclude = { toMatcher(exclude) }
+                    destination = { destination } />;
+}
+
+function item({ path, exclude, ...rest })
+{
+    if (exclude(path))
+        return [];
+
+    const type = { file, directory }[tstat(path)];
+
+    return <type { ...{ path, exclude, ...rest } } />;
+}
+Error.stackTraceLimit = 1000;
+function file({ path, destination, cache, transforms })
+{
+//    const { transform, checksum: transformChecksum } = findTransform.mcall(
+//        refine(state, "find-checksum"), source, transforms) || { };
+console.log(path);
+    if (extname(path) !== ".js")
+        return <copy { ...{ path, destination } }/>;
+
+    const options = { 
+        parserOpts: {
+            "allowReturnOutsideFunction": true,
+            "strictMode": false,
+            "plugins": ["jsx"]
+        },
+        presets: [
+            ["isomorphic-preset", { node: "4.x.x", react: true }]
+        ]
+    };
+
+    return  <copy destination = { destination }>
+                <transform { ...{ cache, path, options } } />
+            </copy>;
+}
+
+function copy({ children:[nested], destination, path = nested.include })
+{
+    copyFileSync(path, destination);
+}
+
+function directory({ path, destination, ...rest })
+{
+    mkdirp(destination);
+
+    return readdirSync(path).map(name =>
+        <item   path = { join(path, name) }
+                destination = { join(destination, name) }
+                { ...rest } />);
+}
+
+function toMatcher(match)
+{
+    if (Array.isArray(match))
+    {
+        const matchers = match.map(toMatcher);
+
+        return path => matchers.some(matcher => matcher(path));
+    }
+
+    if (typeof match === "string")
+    {
+        const matcher = new micromatch.matcher(match);
+
+        return path => matcher(path);
+    }
+
+    if (typeof match === "function")
+        return match;
+
+    return () => false;
+}
+
---
+++ isomorphic-compile/internal/transform.js
@@ -0,0 +1,121 @@
+
+const { basename, join } = require("path");
+const { existsSync, readFileSync, writeFileSync } = require("fs");
+
+const babylon = require("babylon");
+const { transformFromAst } = require("babel-core");
+const File = require("babel-core/lib/transformation/file").default;
+
+const getMerkleChecksum = require("./get-merkle-checksum");
+
+//const metadataPlugin = require("./babel-plugin-metadata");
+//const requiresPlugin = require("./babel-plugin-requires");
+//const entrypointsPlugin = require("./babel-plugin-entrypoints");
+
+
+module.exports = function transform({ cache, path, options })
+{
+    const contents = readFileSync(path, "utf-8");
+
+    return  <transformAST { ...{ cache, contents, filename: path, options } }>
+                <parse contents = { contents } >
+                    <parserOptions options = { options } />
+                </parse>
+            </transformAST>
+}
+
+// It's not worth it to cache these on the filesystem for now. It takes too long
+// to just read and parse the JSON. Perhaps if we could stirp out the tokens, or
+// only cache if its under X KB. But we still get the benefit of caching it in
+// memory.
+function parse({ children:[options], contents })
+{console.log(options);
+options.plugins.push("jsx");
+    return babylon.parse(contents, options);
+}
+
+function parserOptions({ options })
+{
+    return new File(options).parserOpts;
+}
+
+function transformAST({ children:[AST], cache, contents, filename, options })
+{
+    const checksum = getMerkleChecksum({ contents, filename, options })
+        .replace(/\//g, "_");
+    const mutatedOptions = getMutatedOptions(options, checksum, filename);
+    const contentsCachePath = getContentsCachePath(cache, checksum, filename);
+    const metadataCachePath = getMetadataCachePath(cache, checksum, filename);
+
+    if (existsSync(contentsCachePath))
+    {
+        const include = contentsCachePath;
+        const metadata = getCachedMetadata(metadataCachePath);
+
+        return { include, path:filename, ...metadata };
+    }
+
+    const transformed = transformFromAst(AST, contents, mutatedOptions);
+    const transformedContents = transformed.code;
+
+    writeFileSync(contentsCachePath, transformedContents, "utf-8");
+
+    const metadata = transformed.metadata.isomorphic ||
+        { dependencies: new Set(), entrypoints: new Set(), assets: new Set() };
+
+    cacheMetadata(metadataCachePath, metadata);
+
+    return { include: contentsCachePath, path:filename, checksum, ...metadata };
+}
+
+function getMutatedOptions(options, checksum, filename)
+{return options;
+    const plugins = [metadataPlugin, requiresPlugin, entrypointsPlugin]
+        .concat(options.plugins || []);
+
+    return { ...options, plugins, filename };
+}
+
+function cacheMetadata(aPath, metadata)
+{
+    if (!metadata)
+        return;
+
+    if (metadata.dependencies.size <= 0 &&
+        metadata.entrypoints.size <= 0)
+        return;
+
+    const dependencies = Array.from(metadata.dependencies);
+    const entrypoints = Array.from(metadata.entrypoints);
+
+    const asArrays = { dependencies, entrypoints };
+
+    writeFileSync(aPath, JSON.stringify(asArrays, null, 2), "utf-8");
+}
+
+function getCachedMetadata(aPath)
+{
+    if (!existsSync(aPath))
+        return { dependencies: new Set() };
+
+    const metadata = JSON.parse(readFileSync(aPath, "utf-8"));
+    const dependencies = new Set(metadata.dependencies);
+    const entrypoints = new Set(metadata.entrypoints);
+
+    return { dependencies, entrypoints };
+}
+
+function getContentsCachePath(cache, checksum, filename)
+{
+    return getCachePath(cache, checksum, filename, "contents", "js");
+}
+
+function getMetadataCachePath(cache, checksum, filename)
+{
+    return getCachePath(cache, checksum, filename, "metadata", ".json");
+}
+
+function getCachePath(cache, checksum, filename, type, extname)
+{
+    return join(cache, `${basename(filename)}-${type}-${checksum}.${extname}`);
+}
