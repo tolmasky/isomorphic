307dbd8bfdedd2b4d12aab2c3c91429c4fa28375
Randy Luecke <randy.luecke@me.com>
2017-10-30 16:49:25 -0700

    Avoid function call for array / immutable List check.

--- isomorphic-serialize/serializers/gapless-array.js
+++ isomorphic-serialize/serializers/gapless-array.js
@@ -1,20 +1,16 @@
 
-var isImmutable = require("immutable").List.isList;
-// var mapInvoker = require("../utils").invoker("map");
+var IS_LIST_SENTINEL = "@@__IMMUTABLE_LIST__@@";
 
 function serializeGaplessArray(serializedObject, anArray, aContext, toObjectSerialization)
 {
     var i = 0;
 
-    if (isImmutable(anArray))
+    if (anArray[IS_LIST_SENTINEL])
     {
         var count = anArray.size;
 
         for (; i < count; i++)
             serializedObject[i + 1] = toObjectSerialization(anArray.get(i), aContext);
-
-        return serializedObject;
-
     }
     else
     {
@@ -22,9 +18,9 @@ function serializeGaplessArray(serializedObject, anArray, aContext, toObjectSeri
 
         for (; i < count; i++)
             serializedObject[i + 1] = toObjectSerialization(anArray[i], aContext);
-
-        return serializedObject;
     }
+
+    return serializedObject;
 }
 
 module.exports = serializeGaplessArray;
