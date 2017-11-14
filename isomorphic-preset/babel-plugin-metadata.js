bfa5973424af0da444e34ddbcb5a1dadc2d25e19
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2017-11-14 13:36:30 -0800

    Fix globals metadata.
    Reviewed by @tolmasky.

--- isomorphic-preset/babel-plugin-metadata.js
+++ isomorphic-preset/babel-plugin-metadata.js
@@ -9,7 +9,7 @@ module.exports = function (_, options)
         const entrypoints = new Set();
         const assets = new Set();
         const globals = Object.keys(scope.globals)
-            .reduce((globals, key) => (globals[key] = true, globals), scope.globals);
+            .reduce((globals, key) => (globals[key] = true, globals), { });
         const metadata = { ...state.opts, dependencies, entrypoints, assets, globals };
 
         state.file.metadata["isomorphic"] = metadata;
