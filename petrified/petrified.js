af740b2bd84d3c13993a3c9cda7f64064b2d310f
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-01-29 10:51:10 -0800

    Pages.
    Reviewed by @tolmasky.

--- petrified/petrified.js
+++ petrified/petrified.js
@@ -3,26 +3,39 @@ const tree = require("isomorphic-tree");
 
 const page = require("./transform/page");
 const post = require("./transform/post");
-const components = require("./components");
+const getComponents = require("./components");
 
 const express = require("express");
     
 
 module.exports = function ({ source, destination, cache })
 {
-    const options = { components: components({ source }) };
-    const transforms = [{ match:`${source}/posts/*`, transform: post, options }];
+    const components = getComponents({ source });
 
     require("child_process").execSync(`rm -rf ${source}/_cache`);
 
-    console.log(transforms);
-
-runtime([
-        <tree { ...{ source: `${source}/posts`, transforms, destination, cache } }/>,
-        <tree { ...{ source: `${source}/pages`, transforms, destination, cache } }/>,
+    runtime([
+        <posts { ...{ components, source:`${source}/posts`, destination, cache } }/>,
+        <pages { ...{ components, source:`${source}/pages`, destination, cache } }/>
     ]);
-    
-        express()
+
+    express()
         .use("/", express.static(destination))
         .listen(4500);
+}
+
+function pages({ components, source, destination, cache })
+{
+    const options = { components };
+    const transforms = [{ match: page.match, transform: page, options }];
+
+    return <tree { ...{ source, transforms, destination, cache } }/>;
+}
+
+function posts({ components, source, destination, cache })
+{
+    const options = { components };
+    const transforms = [{ match:`${source}/*`, transform: post, options }];
+    
+    return <tree { ...{ source, transforms, destination, cache } }/>;
 }
--- petrified/transform/page.js
+++ petrified/transform/page.js
@@ -1,8 +1,8 @@
-const { basename, extname } = require("path");
+const { basename, dirname, extname } = require("path");
 const transforms = [require("./markdown"), require("./react")];
 
 
-module.exports = function page({ source, ...rest })
+module.exports = function page({ source, destination, ...rest })
 {
     const extension = extname(source);
     const transform = transforms.find(
@@ -11,10 +11,16 @@ module.exports = function page({ source, ...rest })
     if (!transform)
         throw new Error(`Can't render ${basename(source)}`);
 
-    return <transform { ...{ source, ...rest } } />;
+    const name = basename(source, extension);
+    const type = extname(name);
+    const override = type === "html" ?
+        `${dirname(source)}/${name}` :
+        `${dirname(source)}/${basename(name)}/index.html`;
+
+    return <transform { ...{ source, destination: override, ...rest } } />;
 }
 
-module.exports.match = "**/*.(" + transforms
+module.exports.match = "**/*.(page|html).(" + transforms
     .map(transform => Array.from(transform.extensions))
     .map(extensions => extensions.join("|"))
     .join("|") + ")";
